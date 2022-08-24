14f6ccccc4563834e4062d8d366bb8a9f5482944
Chimpicus <caseyboyes@gmail.com>
2022-08-24 08:35:47 +0100

    Frontend initial commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  // fires only once on initial render
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
+    </div>
+  );
+};
+export default Home;
